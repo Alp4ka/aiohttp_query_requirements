469a46884a54e2d4c0613f4ce7fd3fbc49e43841
Roman <saeretpk@gmail.com>
2022-04-02 16:33:06 +0300

    rebuild

--- aiohttp_query_requirements/__init__.py
+++ aiohttp_query_requirements/__init__.py
@@ -1,2 +1,2 @@
-from query_decorators import json_requirements
-from query_decorators import query_requirements
+from aiohttp_query_requirements.query_decorators import json_requirements
+from aiohttp_query_requirements.query_decorators import query_requirements
